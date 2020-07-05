5673b002afe798c5c2c5d65c084c2d01bd0db6f8
Manee Osman <ahmadmo@rcyanbu.gov.sa>
2020-07-05 10:15:57 +0300

    Initial Commit

---
+++ Ruhma/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ruhma
+//
+//  Created by Manee Osman on 7/5/20.
+//  Copyright © 2020 Manee Osman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
